304d8f5bb441781c7c3de767ed4b187777539a2c
aliserhan315 <aliserhan315@gmail.com>
2025-07-18 00:57:41 +0300

    did the agent model and created its factory

--- agent-assignment/app/Models/Agent.php
+++ agent-assignment/app/Models/Agent.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Agent extends Model
 {
-    //
+
+    use HasFactory;
+    public $incrementing = false;
+    protected $keyType = 'uuid';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'code_name',
+        'active',
+    ];
+
 }
